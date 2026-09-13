d09c0aad3d64d8be5273cc84f2ebd60a9ebd75c7
DOZ <105621923+Junhyeong1006@users.noreply.github.com>
2026-09-13 18:16:47 +0900

    edit

---
+++ site-config.js
@@ -0,0 +1,6 @@
+// 공개가 확정된 실제 주소만 입력합니다. 빈 값인 채널은 표시하지 않습니다.
+window.BOMBTEA_CONFIG = {
+  email: "456poiuy31@gmail.com",
+  socials: [], // 예: { label: "YouTube", url: "https://www.youtube.com/@실제계정" }
+  game: { steamUrl: "", trailerUrl: "" }
+};
